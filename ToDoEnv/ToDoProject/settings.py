a4f5b811ea7c82d1a6b31940f204afa696a0629d
HassanAli <hassanali4112@gmail.com>
2024-06-08 14:17:09 +0500

    Added Pagination to API List View

--- ToDoEnv/ToDoProject/settings.py
+++ ToDoEnv/ToDoProject/settings.py
@@ -58,7 +58,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication'],
-    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticatedOrReadOnly']}
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticatedOrReadOnly'],
+    'DEFAULT_PAGINATION_CLASS': "rest_framework.pagination.LimitOffsetPagination",
+    'PAGE_SIZE': 2}
 
 CORS_ORIGIN_ALLOW_ALL = True
 
--- ToDoEnv/todos/serializer.py
+++ ToDoEnv/todos/serializer.py
@@ -2,13 +2,15 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 from .models import Todos
+import json
 
 class TodoSerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='todo-list', lookup_field="pk")
     edit_url = serializers.SerializerMethodField(read_only=True)
+    owner = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Todos
-        fields = ['user','url','edit_url', 'pk', 'task','content' ,'completion']
+        fields = ['owner','url','edit_url', 'pk', 'task','content' ,'completion']
 
     def get_edit_url(self, obj):
         request = self.context.get("request")
@@ -17,3 +19,11 @@ class TodoSerializer(serializers.ModelSerializer):
             return None
         
         return reverse("todo-detail", kwargs={"pk": obj.pk}, request=request)
+    
+    def get_owner(self, obj):
+        data = {}
+
+        data['user'] = obj.user.username
+        data['id'] = obj.user.id
+
+        return data
